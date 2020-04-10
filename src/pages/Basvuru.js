3617ab8f776d6e7473d9223a437a3975f548c77a
yildizadem <ademyildiz9258@gmail.com>
2020-04-10 21:33:45 +0300

    covid güncellemesi yapıldı

--- src/pages/Basvuru.js
+++ src/pages/Basvuru.js
@@ -1,15 +1,15 @@
 import React from 'react';
+import duyuru from "../duyuru.jpg"
 
 const style = {
-    width: "100%",
     height: "calc(100vh - 73.88px)",
     border: "unset"
 }
 function Basvuru() {
     return (
-        <iframe title="basvuru" style={style}
-            src="https://docs.google.com/forms/d/e/1FAIpQLScM8HXQWItlWYS6oOcXjy9J3Zwm7kSul8QIZog7yJWhuWnx5g/viewform"
-        ></iframe>
+        <div style={{ display: "flex", justifyContent: "center", width: "100%" }}>
+            <img src={duyuru} alt="duyuru" style={style} />
+        </div>
     )
 }
 
